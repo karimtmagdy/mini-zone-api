dccdb15f9940051071747c40e9c572149d94f8c6
karimtmagdy <karimtmagdy@gmail.com>
2026-03-03 20:10:41 +0200

    prepar file index to test on vercel

--- src/index.ts
+++ src/index.ts
@@ -2,29 +2,25 @@ import "dotenv/config";
 import express from "express";
 import serverless from "serverless-http";
 // import favicon from "serve-favicon";
-import path from "path";
-import { fileURLToPath } from "url";
 import helmet from "helmet";
-import { Database } from "./config/data/db.js";
-import { logger } from "./lib/logger.js";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(helmet());
 // app.use(favicon(path.join(__dirname, "favicon.ico")));
 
 // Example API endpoint - JSON
-app.get("/api-data", (req, res) => {
-  res.json({
-    message: "Here is some sample API data",
-    items: ["apple", "banana", "cherry"],
+app.get(["/"], (_req, res) => {
+  res.status(200).json({
+    code: 200,
+    status: "success",
+    api: "Mini Zone",
+    message: "Welcome to the Vercel Backend",
+    developer: "karimtmagdy",
+    platform: "Vercel",
+    version: "v1",
+    environment: "production",
+    time: new Date().toISOString(),
   });
-});
-
-// Health check
-app.get("/healthz", (req, res) => {
-  res.status(200).json({ status: "ok", timestamp: new Date().toISOString() });
 });
 
 export { app };
