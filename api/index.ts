698eb3ea6eb5415fca2ee41d0fc2adde496458b5
karimtmagdy <karimtmagdy@gmail.com>
2026-03-03 08:12:41 +0200

    prepar vercel json

--- api/index.ts
+++ api/index.ts
@@ -1,2 +1,25 @@
 import { app } from "../src/index";
 export default app;
+// import express from "express";
+// import { GlobalErrorHandler } from "./global/GlobalErrorHandler";
+// const app = express();
+// const PORT = 8000;
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// app.get("/", (req, res) => {
+//   res.send("<h1></h1>");
+// });
+// GlobalErrorHandler(app);
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
+
+// export { app };
+// // {
+// //   "version": 2,
+// //   "builds": [{ "src": "dist/server.js", "use": "@vercel/node" }],
+// //   "routes": [{ "src": "/(.*)", "dest": "dist/server.js" }],
+// //   "crons": [{ "path": "/src/index.ts", "schedule": "0 10 * * *" }]
+// // }
